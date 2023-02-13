d69456113644a20e5c1f6d775ac53a94b64d9c35
harrymohney <harrison.mohney@gmail.com>
2023-02-13 12:15:07 -0800

    footer added
    Co-authored-by: Taylor M <CatsMeow492@users.noreply.github.com>

---
+++ src/components/footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+
+const Footer = () => (
+  <Container>
+    <Row>
+      <Col>
+        <p>This is some text inside a Bootstrap container!</p>
+      </Col>
+    </Row>
+  </Container>
+);
+
+export default Footer;
